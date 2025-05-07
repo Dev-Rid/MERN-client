8dba91d0be7d949e2d46a9b91c3bce744db80952
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-07 14:03:35 +0100

    feat: all my necessary files changed to jsx

---
+++ src/components/WorkoutForm.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const WorkoutForm = () => {
+    const [title, setTitle] = useState("")
+    const [load, setLoad] = useState("")
+    const [reps, setReps] =    useState("")
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WorkoutForm
