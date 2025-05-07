98ad20f208d6d35406f2fff3c28780d4607e011d
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-07 15:49:53 +0100

    feat: handlick delete

--- src/components/Workoutdetails.jsx
+++ src/components/Workoutdetails.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 const Workoutdetails = ( {workout} ) => {
+  const { dispatch } = useWorkoutsContext()
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    })
+    const json = await response.json()
+    if(!Response.ok){
+      dispatch({type: "DELETE_WORKOUT", payload: json})
+    }
+    // case "DELETE_WORKOUT":
+    //   return {
+    //     workouts: state.workouts.filter()
+    //   }
+
+  }
+
   return (
     <div className='workout-details'>
         <h4>{workout.title}</h4>
         <p><strong>Load (kg):</strong>{workout.load}</p>
         <p><strong>Reps:</strong>{workout.reps}</p>
         <p>{workout.createdAt}</p>
+        <span onClick={handleClick}>DElete</span>
     </div>
   )
 }
