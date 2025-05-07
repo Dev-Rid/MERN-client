9ee59c934e52be8468ad0e478c3db67e765cc4fb
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-07 13:52:28 +0100

    feat: fetching from frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
         <div>
+            <BrowserRouter> 
+            <Navbar/>
+                <div className="pages">
+                    <Routes>
+                        <Route
+                        element={<Home/>}
+                        path="/"
+                        
+                        />
 
+                        
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
     </div>
   );
