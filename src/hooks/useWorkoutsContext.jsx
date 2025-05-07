bcb70f28f1163090e6f4e485576bccf8f8c3a22c
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-07 15:30:21 +0100

    feat: adding react contexts for global management instead of local ones

---
+++ src/hooks/useWorkoutsContext.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import { WorkoutsContext } from "../components/context/WorkoutContext"
+
+
+
+export const useWorkoutsContext = () => {
+    const context = useContext(WorkoutsContext)
+
+    if(!context){
+        throw Error("useWorkoutsContext must be used inside an WorkoutsContextProvider")
+    }
+
+    return context
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Workoutdetails from '../components/Workoutdetails'
 import WorkoutForm from '../components/WorkoutForm'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null)
+  const {workouts, dispatch} = useWorkoutsContext()
 
   useEffect(() => {
     const fetchWorkouts = async () =>{
@@ -11,7 +12,7 @@ const Home = () => {
       const json = await response.json()
    
       if(response.ok){
-        setWorkouts(json)
+        dispatch({type: "SET_WORKOUTS", payload:json})
       }
      }
 
